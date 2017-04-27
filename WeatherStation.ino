b03817361372ce15c38136fe44c52bb9eb55e737
James Morris <jamesmorris2@gmail.com>
2017-04-27 14:42:27 -0400

    Created timer to update indoor weather information every 5 seconds.

--- WeatherStation.ino
+++ WeatherStation.ino
@@ -1,14 +1,47 @@
 #include <Temboo.h>
 #include <BlynkSimpleEsp8266.h>
 #include "BlynkCreds.h"
+#include "SparkFun_Si7021_Breakout_Library.h"
+#include <Wire.h>
+#include <SimpleTimer.h>
+
+
+
+#define BLYNK_PRINT Serial
+
+float humidity = 0;
+float tempf = 0;
+
+SimpleTimer timer;
+
+Weather sensor;
 
 void setup() {
-  startNTP();
-  startWiFi();
-  Blynk.config(BLYNK_AUTH);
+  Serial.begin(9600);
+//  startWiFi();
+//  startNTP();
+//  Blynk.config(BLYNK_AUTH);
+  sensor.begin();
+  updateIndoor();
+  timer.setInterval(5 * 1000, updateIndoor);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Blynk.run();
+  timer.run();
 }
+
+void updateIndoor() {
+  tempf = sensor.getTempF();
+  humidity = sensor.getRH();
+  
+  Serial.print("Indoor temp: ");
+  Serial.print(tempf);
+  Serial.print("F, ");
+
+  Serial.print("Indoor humidity: ");
+  Serial.print(humidity);
+  Serial.println("%");
+  Blynk.virtualWrite(V1, tempf);
+}
+
