b625f9b8f6a74f84abaff5248d6a766e870bcaa2
James Morris <jamesmorris2@gmail.com>
2017-05-01 15:05:34 -0400

    Comments in default headers

--- BlynkCreds.default.h
+++ BlynkCreds.default.h
@@ -1,1 +1,3 @@
+// Rename this file to BlynkCreds.h and then fill in the auth key
+
 const char BLYNK_AUTH[] = "";
--- WiFiCreds.default.h
+++ WiFiCreds.default.h
@@ -1,2 +1,4 @@
+// Rename this file to WiFiCreds.h and then fill in your SSID and password
+
 const char* ssid = "";
 const char* pass = "";
