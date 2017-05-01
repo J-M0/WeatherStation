8e2a8f0ab9c1d95ed9c4963d5504eb873db65c9c
James Morris <jamesmorris2@gmail.com>
2017-05-01 16:04:03 -0400

    More credential files

---
+++ GoogleCreds.default.h
@@ -0,0 +1,6 @@
+// Rename this file to GoogleCreds.h and then fill in the information
+
+const char* G_REFRESH_TOKEN = "";
+const char* G_CLIENT_SECRET = "";
+const char* G_CLIENT_ID = "";
+const char* G_CLIENT_ID = "";
---
+++ TembooAccount.default.h
@@ -0,0 +1,5 @@
+// Rename this file to TembooAccount.h and then fill in the information
+
+const char* TEMBOO_ACCOUNT = "";
+const char* TEMBOO_APP_KEY_NAME = "";
+const char* TEMBOO_APP_KEY = "";
